9fdb2cc86896ce46df9c95189d8a4a2aac1a3fa6
Amrsamy19 <amrsamy622@gmail.com>
2022-05-24 20:04:31 +0200

    Finished styling login page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,62 +1,70 @@
-const Login = () => {
+const LoginForm = () => {
 	return (
-		<div className="h-screen flex flex-col items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-			<div className="m-auto bg-white shadow-2xl rounded p-16 bg-green-600">
-				<div className="max-w-md w-full space-y-8">
-					<h1 className="mt-6 text-center text-5xl font-Comfortaa font-semibold text-green-600">
-						Plastikat
-					</h1>
+		<div className="p-20 flex flex-col items-center rounded-lg shadow-gray-900">
+			<div className="w-fit space-y-8">
+				<h1 className="mt-6 text-center text-5xl font-Comfortaa font-normal text-green-600">
+					Plastikat Board
+				</h1>
+			</div>
+			<form className="mt-8 space-y-6">
+				<div className="relative z-0 w-full group">
+					<input
+						type="email"
+						id="email"
+						className="block py-2.5 px-0 w-full text-lg font-Comfortaa text-gray-900 bg-transparent border-0 border-b-2 border-green-600 focus:outline-none focus:ring-0 focus:border-green-700 peer"
+						placeholder=""
+						required
+					/>
+					<label
+						htmlFor="email"
+						className="peer-focus:font-medium absolute font-Comfortaa text-lg text-gray-700 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+					>
+						Email
+					</label>
 				</div>
-				<form className="mt-8 space-y-6">
-					<div className="relative z-0 w-full mb-6 group">
-						<input
-							type="email"
-							name="email"
-							id="email"
-							className="block py-2.5 px-0 w-full text-lg font-Comfortaa text-gray-900 bg-transparent border-0 border-b-2 border-green-600 appearance-none focus:outline-none focus:ring-0 focus:border-green-700 peer"
-							placeholder=""
-							required
-						/>
-						<label
-							for="email"
-							className="peer-focus:font-medium absolute font-Comfortaa text-lg text-gray-700 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-						>
-							Email
-						</label>
-					</div>
-					<div className="relative z-0 w-full mb-6 group">
-						<input
-							type="password"
-							name="password"
-							id="password"
-							className="block py-2.5 px-0 w-full font-Comfortaa text-lg text-gray-900 bg-transparent border-0 border-b-2 border-green-600 appearance-none focus:outline-none focus:ring-0 focus:border-green-700 peer"
-							placeholder=""
-							minLength={8}
-							required
-						/>
-						<label
-							for="password"
-							className="peer-focus:font-medium font-Comfortaa absolute text-lg text-gray-700 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-						>
-							Password
-						</label>
-					</div>
-					<div className="relative z-0 w-full mb-6 group">
-						<button
-							type="submit"
-							className="group relative w-full flex justify-center
-							py-2 px-4 border border-transparent text-lg font-Comfortaa font-medium
+				<div className="relative z-0 w-full group">
+					<input
+						type="password"
+						id="password"
+						className="block py-2.5 px-0 w-full font-Comfortaa text-lg text-gray-900 bg-transparent border-0 border-b-2 border-green-600 focus:outline-none focus:ring-0 focus:border-green-700 peer"
+						placeholder=""
+						minLength={8}
+						required
+					/>
+					<label
+						htmlFor="password"
+						className="peer-focus:font-medium font-Comfortaa absolute text-lg text-gray-700 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+					>
+						Password
+					</label>
+				</div>
+				<div className="relative z-0 w-full group">
+					<button
+						type="submit"
+						className="transition duration-150 relative w-full py-2 px-4 
+							border border-transparent text-lg font-Comfortaa font-medium
 							rounded-md text-white bg-green-600 hover:bg-green-700
 							focus:outline-none focus:ring-2 focus:ring-offset-2
 							focus:ring-green-500"
-						>
-							Login
-						</button>
-					</div>
-				</form>
+					>
+						Login
+					</button>
+				</div>
+			</form>
+		</div>
+	);
+};
+
+const LoginCard = () => {
+	return (
+		<div className="w-screen h-screen flex flex-col items-center justify-center">
+			<div className="bg-green-600 w-full h-full p-8">
+				<div className="bg-white flex flex-col items-center justify-center w-full h-full p-24">
+					<LoginForm />
+				</div>
 			</div>
 		</div>
 	);
 };
 
-export default Login;
+export default LoginCard;
